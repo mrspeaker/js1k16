0bb26592837b7e621070788b58b8e05c4b412509
Mr Speaker <mrspeaker@gmail.com>
2016-03-04 12:47:02 -0500

    let's make this easier

--- README.md
+++
@@ -1,8 +0,0 @@
-# ES6 + budo
-
-ES6 (+ browserify)
-
-* npm install
-* npm start
-
-Browse on http://localhost:9966
--- index.js
+++
@@ -1,3 +0,0 @@
-var a = require("./test2");
-var tex = document.currentScript;
-console.log("text", tex.innerHTML);
